fa50e0b0c39c35f17d6f8aed338ea6da5526229f
Pranath04 <medanarayanapranath@gmail.com>
2023-07-15 22:30:35 +0530

    Given an integer array A of size N and an integer B, you have to return the same array after rotating it B times towards the right.

---
+++ Arrayrotation.java
@@ -0,0 +1,29 @@
+package Array_Rotation;
+public class Arrayrotation{
+	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter array size");
+		int arrsize = scanner.nextInt();
+		System.out.println("Enter array elements");
+		int arr[]=new int[arrsize];
+		for(int i=0;i<arrsize;i++) {
+			arr[i]=scanner.nextInt();
+		}
+		int r=0;
+		scanner.close();
+		int ans[]=reverseing(r,arrsize,arr);
+		for(int i=0;i<ans.length;i++){
+	        System.out.println(ans[i]);
+	    }
+	}
+	static int[] reverseing(int r,int arrsize,int arr[]) {
+		while(arrsize-1>r) {
+			int temp=arr[arrsize-1];
+            arr[arrsize-1]=arr[r];
+            arr[r]=temp;
+			r++;
+			arrsize--;
+ 		}
+		return arr;
+	}
+}
